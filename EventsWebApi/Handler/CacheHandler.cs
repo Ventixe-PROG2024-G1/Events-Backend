6fc39ce2082f5eb674c4c93b53b2ddb20209f030
RamiObaid92 <163041066+RamiObaid92@users.noreply.github.com>
2025-05-05 00:44:20 +0200

    CacheHandler quickfix

--- EventsWebApi/Handler/CacheHandler.cs
+++ EventsWebApi/Handler/CacheHandler.cs
@@ -7,14 +7,29 @@ public interface ICacheHandler<T>
     T? GetFromCache(string cacheKey);
     void RemoveCache(string cacheKey);
     T SetCache(string cacheKey, T data, int minutesToCache = 10);
+    Task<T?> GetOrCreateAsync(string cacheKey, Func<Task<T?>> factory, int minutesToCache = 10);
 }
 
 public class CacheHandler<T>(IMemoryCache cache) : ICacheHandler<T>
 {
     private readonly IMemoryCache _cache = cache;
 
+    public async Task<T?> GetOrCreateAsync(string cacheKey, Func<Task<T?>> factory, int minutesToCache = 10)
+    {
+        return await _cache.GetOrCreateAsync(cacheKey, async cacheEntry =>
+        {
+            cacheEntry.AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(minutesToCache);
+
+            T? data = await factory();
+            return data;
+        });
+    }
+
     public T? GetFromCache(string cacheKey)
-        => _cache.Get<T>(cacheKey);
+    {
+        _cache.TryGetValue(cacheKey, out T? data);
+        return data;
+    }
 
     public T SetCache(string cacheKey, T data, int minutesToCache = 10)
     {
