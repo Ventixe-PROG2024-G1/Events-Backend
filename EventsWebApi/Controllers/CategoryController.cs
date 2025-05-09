1bb1d370ae86d0e171e7fab19b1613e02fe0380c
RamiObaid92 <163041066+RamiObaid92@users.noreply.github.com>
2025-05-09 15:14:22 +0200

    GetAll Category in controller

--- EventsWebApi/Controllers/CategoryController.cs
+++ EventsWebApi/Controllers/CategoryController.cs
@@ -4,26 +4,36 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 
-namespace EventsWebApi.Controllers
+namespace EventsWebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CategoryController(ICategoryService categoryService) : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class CategoryController(ICategoryService categoryService) : ControllerBase
+    private readonly ICategoryService _categoryService = categoryService;
+
+    [HttpPost]
+    public async Task<IActionResult> CreateCategory(CreateCategoryRequest requestData)
     {
-        private readonly ICategoryService _categoryService = categoryService;
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
 
-        [HttpPost]
-        public async Task<IActionResult> CreateCategory(CreateCategoryRequest requestData)
-        {
-            if (!ModelState.IsValid)
-                return BadRequest(ModelState);
+        var result = await _categoryService.CreateCategoryAsync(requestData);
 
-            var result = await _categoryService.CreateCategoryAsync(requestData);
+        if (result == null)
+            return BadRequest("Failed to create category");
 
-            if (result == null)
-                return BadRequest("Failed to create category");
+        return Ok(result);
+    }
 
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllCategories()
+    {
+        var result = await _categoryService.GetAllCategoriesAsync();
+        if (result == null)
             return Ok(result);
-        }
+
+        return Ok(result);
     }
 }
